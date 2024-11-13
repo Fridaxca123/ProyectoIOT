16637d039dd04ee4675ad5b4a0f784fec6a90c7c
Frida Vargas <82528468+Fridaxca123@users.noreply.github.com>
2024-11-13 08:25:20 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,31 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
-// Obtener la fecha actual
-const dateElement = document.getElementById('current-date');
-const today = new Date();
-const options = { year: 'numeric', month: 'long', day: 'numeric' };
+//Actualizacion de datos 
+<script>
+function actualizarDatos() {
+    // Realizar la solicitud AJAX
+    fetch('getData.php')
+        .then(response => response.json())
+        .then(data => {
+            if (!data.error) {
+                // Actualizar los elementos HTML con los datos recibidos
+                document.getElementById("temperatura").innerText = `Temperatura: ${data.temperatura}°C`;
+                document.getElementById("humedad").innerText = `Humedad: ${data.humedad}%`;
+                document.getElementById("uv").innerText = `UV: ${data.uv}`;
+            } else {
+                console.error(data.error);
+            }
+        })
+        .catch(error => console.error('Error al obtener datos:', error));
+}
 
-// Formatear la fecha y actualizar el contenido del elemento
-dateElement.textContent = today.toLocaleDateString('en-US', options);
+// Llamar a actualizarDatos cada 5 segundos para actualizar en tiempo real
+setInterval(actualizarDatos, 5000);
+
+// Ejecutar actualizarDatos cuando se carga la página por primera vez
+document.addEventListener("DOMContentLoaded", actualizarDatos);
+</script>
+
 
 
